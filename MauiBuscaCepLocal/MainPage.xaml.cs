5efcd01ef99333f0bbadd84b42ff6f5ec28a0b39
Luiz <otavio.tata.morais@gmail.com>
2024-09-24 12:32:32 -0300

    first commit

---
+++ MauiBuscaCepLocal/MainPage.xaml.cs
@@ -0,0 +1,29 @@
+﻿using MauiAppBuscaCepLocal.Model;
+using MauiBuscaCepLocal.Services;
+
+namespace MauiBuscaCepLocal
+{
+    public partial class MainPage : ContentPage
+    {
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private async void Button_Clicked(object sender, EventArgs e)
+        {
+            try
+            {
+                Endereco? dados_endereco = await DataService.GetEnderecoByCep(txt_cep.Text);
+
+                BindingContext = dados_endereco;
+
+            }
+            catch (Exception ex)
+            {
+                await DisplayAlert("Ops", ex.Message, "OK");
+            }
+        }
+    }
+}
---
+++ MauiBuscaCepLocal/Services/DataService.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+using MauiAppBuscaCepLocal.Model;
+
+namespace MauiBuscaCepLocal.Services
+{
+    public class DataService
+    {
+        public static async Task<Endereco?> GetEnderecoByCep(string cep)
+        {
+            Endereco? end;
+            using(HttpClient client = new HttpClient())
+            {
+                HttpResponseMessage res = await client.GetAsync("http://localhost:8000/endereco/by-cep?cep=" + cep);
+
+                if (res.IsSuccessStatusCode)
+                {
+                    string json = res.Content.ReadAsStringAsync().Result;
+
+                    end = JsonConvert.DeserializeObject<Endereco>(json);
+                }
+                else
+                {
+                    throw new Exception(res.RequestMessage.Content.ToString());
+                }
+                return end;
+            } 
+        }
+    }
+}
